4c6981f3957a0845f8499e76ef838d2ab090a8bd
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-05-17 22:24:48 -0500

    xd

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,5 @@
 import Consola from "~/shared/molecules/consola";
 import type { Route } from "./+types/home";
-import Command from "~/shared/atoms/command";
 import data from "./data";
 
 export function meta({}: Route.MetaArgs) {
@@ -12,14 +11,19 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
 	return (
-		<>
-			<Consolas />
-		</>
+		<div className="h-[100vh] flex justify-center items-center">
+			<div
+				className="flex justify-center items-center flex-wrap gap-y-4 gap-x-8 
+					   max-w-[95%] max-h-[70%] overflow-auto"
+			>
+				<Consolas />
+			</div>
+		</div>
 	);
 }
 const Consolas = () => {
 	return data.flatMap((x) => (
-		<Consola title={x.title} width={"w-[600px]"}>
+		<Consola title={x.title} width={"w-[570px]"}>
 			{x.children}
 		</Consola>
 	));
